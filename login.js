e41f6038e7c290c93c3782442459dce9e879634b
Polygeekism <irishmangeek@gmail.com>
2021-02-12 09:41:02 -0600

    we have page movement!

--- login.js
+++ login.js
@@ -1,1 +1,5 @@
 console.log("congrats, you have activated the JS file.");
+
+function advancePage(){
+    window.location.href = ("./order.html");
+}
---
+++ order.js
@@ -0,0 +1,1 @@
+console.log("heyo this works now too, if only we could reach this page.");
