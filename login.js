96eb48eb00ad1b1839506cbf6c4c16d6f0bb94e2
Polygeekism <irishmangeek@gmail.com>
2021-02-12 09:54:26 -0600

    adding validation to inputs on html form

--- login.js
+++ login.js
@@ -1,5 +1,16 @@
 console.log("congrats, you have activated the JS file.");
 
-function advancePage(){
-    window.location.href = ("./order.html");
+function advancePage() {
+  window.location.href = "./order.html";
+}
+
+var element = document.getElementById("form");
+if (element.addEventListener) {
+  element.addEventListener("submit", validate.true);
+}
+
+function validate(e) {
+  console.log(e);
+  e.preventDefault();
+  console.log("name", e.target[0].value);
 }
