91bc3b93752770f54d29847f9d99920ef800aa77
Polygeekism <irishmangeek@gmail.com>
2021-02-12 10:35:58 -0600

    field validation in place, though messages not setup

--- login.js
+++ login.js
@@ -4,22 +4,44 @@ console.log("congrats, you have activated the JS file.");
 //   window.location.href = "./order.html";
 // }
 
-window.onload = function(){
-    var element = document.getElementById("form");
-    console.log(element);
-    if(element.addEventListener){
-        element.addEventListener("submit", validate, true);
-    }
+window.onload = function () {
+  var element = document.getElementById("form");
+  if (element.addEventListener) {
+    element.addEventListener("submit", validate, true);
+  }
 
-    function validate(e){
-        console.log("e", e);
-        e.preventDefault();
-        console.log("name", e.target[0].value);
-        checkName(e.target[0].value);
-
+  function validate(e) {
+    console.log("e", e);
+    e.preventDefault();
+    var nameInput = e.target[0].value;
+    var emailInput = e.target[1].value;
+    console.log("name", nameInput);
+    checkName(nameInput);
+    console.log("email", emailInput);
+    checkEmail(emailInput);
+    if (checkName(nameInput) && checkEmail(emailInput)) {
+      console.log("both inputs are valid");
+      advancePage();
+    } else {
+      console.log("please use the correct input formats");
     }
-    function checkName(name){
-        var regex = new RegExp(/^[a-zA-Z0-9_.-]*$[a-zA-Z_.-]*$/g);
-        console.log(regex.test(name));
-    }
-}
+  }
+  function checkName(name) {
+    var regex = new RegExp(/^[a-zA-Z0-9_.-]*$[a-zA-Z_.-]*$/g);
+    //console.log(regex.test(name));
+    result = regex.test(name);
+    console.log("result", result);
+    return result;
+  }
+  function checkEmail(email) {
+    var eRegex = new RegExp(
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+    );
+    result = eRegex.test(email);
+    console.log("result", result);
+    return result;
+  }
+  function advancePage() {
+    window.location.href = "./order.html";
+  }
+};
